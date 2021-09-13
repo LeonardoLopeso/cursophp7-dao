5d54d0faaf0f01637db658bbebc7ce23873d70ef
LeoLopeso <leonardo.lopes@corpstek.com.br>
2021-09-13 15:48:43 -0300

    inserir e alterar

--- class/Usuario.php
+++ class/Usuario.php
@@ -62,15 +62,6 @@ class Usuario {
 
 	}
 
-	public function setData($data){
-
-		$this->setIdusuario($data['idusuario']);
-		$this->setDeslogin($data['deslogin']);
-		$this->setDessenha($data['dessenha']);
-		$this->setDtcadastro(new DateTime($data['dtcadastro']));
-
-	}
-
 	public static function search($login) {
 		$sql = new Sql();
 		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
@@ -88,17 +79,57 @@ class Usuario {
 		));
 
 		if(count($results) > 0) {
-			$row = $results[0];
 
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
+
 		}else{
 			throw new Exception("Login e/ou senha inválidos.");
 			
 		}
 
+	}
+
+	public function setData($data) {
+
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro($data['dtcadastro']);
+	}
+
+	public function insert() {
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if(count($results) > 0) {
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public function update($login, $password) {
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+	}
+
+	public function __construct($login = "", $password = "") {
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
 	}
 
 	public function __toString(){
@@ -107,7 +138,7 @@ class Usuario {
 			"idusuario"=>$this->getIdusuario(),
 			"deslogin"=>$this->getDeslogin(),
 			"dessenha"=>$this->getDessenha(),
-			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			"dtcadastro"=>date('d/m/Y H:i:s', strtotime($this->getDtcadastro()))
 		));
 
 	}
