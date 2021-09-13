2cff0be7e45b89f2ad35e31ecf221be7eb688f89
LeoLopeso <leonardo.lopes@corpstek.com.br>
2021-09-13 11:53:44 -0300

    Criando os métodos para listar

--- class/Usuario.php
+++ class/Usuario.php
@@ -55,7 +55,12 @@ class Usuario {
 
 	}
 
-	
+	public static function getList() {
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
 
 	public function setData($data){
 
@@ -63,6 +68,36 @@ class Usuario {
 		$this->setDeslogin($data['deslogin']);
 		$this->setDessenha($data['dessenha']);
 		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public static function search($login) {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results) > 0) {
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+			throw new Exception("Login e/ou senha inválidos.");
+			
+		}
 
 	}
 
--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 
 require_once("config.php");
 
+//Carrega um usuário
+// $root = new Usuario();
+// $root->loadById(1);
+// echo $root;
+
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+//Carrega uma lista de usuáris buscando pelo login
+// $search = Usuario::search("leo");
+// echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
 $usuario = new Usuario();
-
-$usuario->loadById(7);
-
-$usuario->delete();
-
+$usuario->login("root", "#1234");
 echo $usuario;
  ?>
